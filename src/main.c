f2a88b89c5e67063be0edb8e67604e75255b0d50
Guibones123 <guilherme.ff108@gmail.com>
2025-10-30 20:03:38 -0300

    adição do botão de pedestres

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/logging/log.h>
-
+#define BUTTON_NODE DT_NODELABEL(user_button_0)
 LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
 
 #define STACK_SIZE 1024
@@ -16,15 +16,22 @@ LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
 /* Device-tree LED specs */
 static const struct gpio_dt_spec led_verde = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 static const struct gpio_dt_spec led_vermelho = GPIO_DT_SPEC_GET(DT_ALIAS(led2), gpios);
-
+static const struct gpio_dt_spec botao = GPIO_DT_SPEC_GET(BUTTON_NODE, gpios);
+static struct gpio_callback botao_data;
 /* --- Sincronização com Semáforos --- */
 
 
 K_SEM_DEFINE(semaforo_verde, 0, 1);
-
+K_SEM_DEFINE(semaforo_botao, 0, 1);
 K_SEM_DEFINE(semaforo_vermelho, 1, 1);
 int MODO_NOTURNO = 0;
-
+void botao_pressionado(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+    /* Apenas "dá" o semáforo. A thread que o espera vai acordar. */
+    /* É seguro chamar k_sem_give de dentro de uma ISR */
+    k_sem_give(&semaforo_botao);
+    LOG_INF("Botão pressionado!");
+}
 /* Thread A: Controla o LED Verde */
 void thread_verde(void *p1, void *p2, void *p3)
 {
@@ -65,6 +72,7 @@ void thread_verde(void *p1, void *p2, void *p3)
             k_msleep(VERDE_LIGADO); // Espera 4 segundos
 
             /* 4. Passa o bastão para a Thread B (k_sem_give) */
+           
             
         }
         else
@@ -94,16 +102,13 @@ void thread_vermelho(void *p1, void *p2, void *p3)
         k_sem_take(&semaforo_vermelho, K_FOREVER);
         if (MODO_NOTURNO == 0)
         {
+            k_sem_reset(&semaforo_botao);
             /* 1. Espera pela sua vez (k_sem_take) */
             /* Esta linha bloqueia a thread até a Thread A liberar */
             /* 2. Realiza sua ação */
             gpio_pin_set_dt(&led_verde, 0);
             gpio_pin_set_dt(&led_vermelho, 1);
-
-            /* 3. Espera o tempo do LED aceso */
-            k_msleep(VERMELHO_LIGADO); // Espera 2 segundos
-
-            /* 4. Passa o bastão de volta para a Thread A (k_sem_give) */
+            k_sem_take(&semaforo_botao, K_MSEC(VERMELHO_LIGADO));
         }
         else
         {   
@@ -125,5 +130,9 @@ K_THREAD_DEFINE(thread_b_id, STACK_SIZE, thread_vermelho,
 
 void main(void)
 {
+    gpio_pin_configure_dt(&botao, GPIO_INPUT | GPIO_PULL_UP);
+    gpio_pin_interrupt_configure_dt(&botao, GPIO_INT_EDGE_FALLING);
+    gpio_init_callback(&botao_data, botao_pressionado, BIT(botao.pin));
+    gpio_add_callback(botao.port, &botao_data);
     LOG_INF("Main terminou. Threads com Semáforos (4s/2s) estao rodando.");
 }
